ba0ace80e60a7e725c8f29e9a559b54c4b81ae5d
aaronwinter <eatounno@uwaterloo.ca>
2014-12-04 23:37:04 +0000

    branch for todolist

--- inject.js
+++
@@ -1,4 +0,0 @@
-var frame = document.createElement("frame");
-frame.src  = chrome.extension.getURL("addremove.html");
-var div    = document.getElementById("_li")[1][4][0][0][0][2][0];
-div.appendChild(frame);
