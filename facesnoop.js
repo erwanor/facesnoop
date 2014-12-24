147aa489f9e0077d16b486f1c30763f31649b4ab
Pranav <anandpranavv@gmail.com>
2014-12-24 00:45:40 -0500

    Sends new AJAX request when send_messages is called
    Still not complete and working. Need to find out what to put as the argument for .send function.

--- facesnoop.js
+++ facesnoop.js
@@ -10,16 +10,19 @@ var debug = 0;
 
 chrome.webRequest.onBeforeRequest.addListener(
   function(data) {
+    var xmlhttp;
     console.log("INTERCEPTED");
-
+    
     if(debug)
       console.log("data.url:::" + data.url);
       
-    if(data.url == "https://www.facebook.com/ajax/mercury/send_messages.php") //if you send messages, then it shows as seen 
-      return {cancel: false};                                                 //and notifications are dismissed this way
-
-    if(data.url == "https://www.facebook.com/ajax/mercury/change_read_status.php") 
+    if(data.url == "https://www.facebook.com/ajax/mercury/send_messages.php") {//if you send messages, then it shows as seen 
+        xmlhttp.open("POST","change_read_status.php",true);
+        xmlhttp.send();                                                //and notifications are dismissed this way
+    }
+    if(data.url == "https://www.facebook.com/ajax/mercury/change_read_status.php") {
       return {cancel: true};
+    }
 
     else
       return null;
