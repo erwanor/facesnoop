68f9dd86b7c6c8171462d8c8e0c5d2b79098901e
Pranav <anandpranavv@gmail.com>
2014-12-24 00:54:16 -0500

    Variable stops AJAX call being cancelled
    Global variable which stops AJAX call being cancelled put in place. More elegant solution for future needed.

--- facesnoop.js
+++ facesnoop.js
@@ -7,24 +7,30 @@
  * */
 console.log("Init");
 var debug = 0;
+var runIf = 1;
 
 chrome.webRequest.onBeforeRequest.addListener(
   function(data) {
     var xmlhttp;
+    
     console.log("INTERCEPTED");
     
     if(debug)
       console.log("data.url:::" + data.url);
       
     if(data.url == "https://www.facebook.com/ajax/mercury/send_messages.php") {//if you send messages, then it shows as seen 
+        runIf = 0;
         xmlhttp.open("POST","change_read_status.php",true);
         xmlhttp.send();                                                //and notifications are dismissed this way
     }
     if(data.url == "https://www.facebook.com/ajax/mercury/change_read_status.php") {
-      return {cancel: true};
+      if (runIf)
+        runIf = 1;
+        return {cancel: true};
     }
 
     else
+      runIf = 1;
       return null;
   },
 
