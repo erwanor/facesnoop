b20b588e19bf8e4efa3c7d64743423e964f820d1
Pranav <anandpranavv@gmail.com>
2015-01-07 20:20:06 -0500

    facesnoop for firefox

--- facesnoop_firefox.js
+++ facesnoop_firefox.js
@@ -1,79 +1,28 @@
-console.log("Init");
-chrome.webRequest.onBeforeRequest.addListener(
-  function(data) {
-    console.log("INTERCEPTED");
-    console.log("data.url:::" + data.url);
+Services.obs.addObserver(myobserve, 'http-on-modify-request', false);
 
-    if(data.url == "https://www.facebook.com/ajax/mercury/change_read_status.php"){
-      console.log("KILLED");
-      return {cancel: true};
-    }
-
-    else
-      return null;
-  },
-
-
-
-  observe : function(aSubject, aTopic, aData) {
-  if (TOPIC_MODIFY_REQUEST == aTopic) {
-    let url;
-
-    aSubject.QueryInterface(Components.interfaces.nsIHttpChannel);
-    url = aSubject.URI.spec;
-
-    if (RE_URL_TO_MODIFY.test(url)) { // RE_URL_TO_MODIFY is a regular expression.
-      aSubject.setRequestHeader("Referer", "http://example.com", false);
-    } else if (RE_URL_TO_CANCEL.test(url)) { // RE_URL_TO_CANCEL is a regular expression.
-      aSubject.cancel(Components.results.NS_BINDING_ABORTED);
-    }
-  }
-}
-
-const {classes: Cc, interfaces: Ci, utils: Cu, results: Cr} = Components;
+var {utils: Cu, classes: Cc, instances: Ci, results: Cr} = Components
 Cu.import('resource://gre/modules/Services.jsm');
+var myobserve = function(aSubject, aTopic, aData) {
+    var httpChannel = aSubject.QueryInterface(Ci.nsIHttpChannel);
 
-var urls_block = [ //if urls ontain any of these elements they will be blocked or redirected, your choice based on code in observer line 17
- 'https://www.facebook.com/ajax/mercury/change_read_status.php',
-];
+    var isXHR;
 
-var observers = {
-    'http-on-modify-request': {
-        observe: function (aSubject, aTopic, aData) {
-            console.info('http-on-modify-request: aSubject = ' + aSubject + ' | aTopic = ' + aTopic + ' | aData = ' + aData);
-            var httpChannel = aSubject.QueryInterface(Ci.nsIHttpChannel);
-            var requestUrl = httpChannel.URI.spec;
-            for (var i=0; i<urls_block.length; i++) {
-             if (requestUrl.indexOf(urls_block[i]) > -1) {
-              httpChannel.cancel(Cr.NS_BINDING_ABORTED); //this aborts the load
-              //httpChannel.redirectTo(Services.io.newURI('data:text,url_blocked', null, null)); //can redirect with this line, if dont want to redirect and just block, then uncomment this line and comment out line above (line 17)
-              break;
-             }
-            }
-        },
-        reg: function () {
-            Services.obs.addObserver(observers['http-on-modify-request'], 'http-on-modify-request', false);
-        },
-        unreg: function () {
-            Services.obs.removeObserver(observers['http-on-modify-request'], 'http-on-modify-request');
+    try {
+        var callbacks = httpChannel.notificationCallbacks;
+        var xhr = callbacks ? callbacks.getInterface(Ci.nsIXMLHttpRequest) : null;
+        isXHR = !!xhr;
+    } catch (e) {
+        isXHR = false;
+    }
+
+    if (isXHR) {
+        //its ajax
+        var requestUrl = httpChannel.URI.spec;
+        if (requestUrl.indexOf('https://www.facebook.com/ajax/mercury/change_read_status.php') > -1) {
+            httpChannel.cancel(Cr.NS_BINDING_ABORTED);
         }
     }
-};
-
-function install() {}
-
-function uninstall() {}
-
-function startup() {
- for (var o in observers) {
-  observers[o].reg();
- }
 }
- 
-function shutdown(aData, aReason) {
- if (aReason == APP_SHUTDOWN) return;
- 
- for (var o in observers) {
-  observers[o].unreg();
- }
-}
+
+
+Services.obs.removeObserver(myobserve, 'http-on-modify-request', false);
